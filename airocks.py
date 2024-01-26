2132627e14109011d21b080635d9f3159ba7fef0
Karun Subramanian <kas7176@g.harvard.edu>
2024-01-26 16:06:36 -0600

    Added code to print current time

--- airocks.py
+++ airocks.py
@@ -1,4 +1,7 @@
 # Print the string "AI Rocks!" in standard out
-
+import datetime
 print("AI Rocks forever!")
 print("Machine Learning is Sweet!")
+
+# Print the current time
+print(datetime.datetime.now())
