61437fd18763e107888adfdc7b7889192b68c656
v-post <veronikapost1@gmail.com>
2024-09-23 19:33:31 -0400

    Add refactoring in class

--- gilded_rose.py
+++ gilded_rose.py
@@ -6,34 +6,55 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
+        aged_brie = "Aged Brie"
+        legendary_item = "Sulfuras, Hand of Ragnaros"
+        backstage_pass = "Backstage passes to a TAFKAL80ETC concert"
+        conjured = "Conjured"
+
+        max_quality_increase = 50
+
         for item in self.items:
-            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality < 50:
-                    item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
-            else:
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality -= 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in -= 1
-            if item.sell_in < 0:
-                if item.name == "Aged Brie":
-                    if item.quality < 50:
-                        item.quality += 1
+
+            if item.name == aged_brie:
+                if item.sell_in >= 0:
+                    quality_delta = 1
                 else:
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = item.quality - item.quality
-                    else:
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality -= 1
+                    quality_delta = 2
+
+                # Decrement sell in time
+                item.sell_in -= 1
+
+            elif item.name == legendary_item:
+                quality_delta = 0
+
+            elif item.name == backstage_pass:
+                if item.sell_in <= 0:
+                    quality_delta = -1 * item.quality
+                elif item.sell_in <= 5:
+                    quality_delta = 3
+                elif item.sell_in <= 10:
+                    quality_delta = 2
+                else:
+                    quality_delta = 1
+
+                item.sell_in -= 1
+
+            # The rest of the items' behavior
+            else:
+                if item.sell_in <= 0:
+                    quality_delta = -2
+                else:
+                    quality_delta = -1
+
+                item.sell_in -= 1
+
+
+            # We shouldn't be able to exceed 50
+            if item.quality + quality_delta <= max_quality_increase:
+                item.quality += quality_delta
+            # Quality cannot go negative
+            if item.quality < 0:
+                item.quality = 0
 
 
 class Item:
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -7,6 +7,7 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
+        # Add a test comment
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
